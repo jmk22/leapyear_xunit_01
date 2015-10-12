43c6e1e91153c50a25959b05ae64b4174f688b0a
Jill Kuchman <pillanoid@gmail.com>
2015-10-12 16:52:55 -0700

    Add initial files with working tests

---
+++ LeapYearTest.cs
@@ -0,0 +1,33 @@
+using Xunit;
+
+namespace LeapYear
+{
+    public class LeapYearTest
+    {
+        [Fact]
+        public void IsLeapYear_ForNumberDivisibleByFour_true()
+        {
+          LeapYear LeapYear = new LeapYear();
+          Assert.Equal(true, LeapYear.IsLeapYear(2012));
+        }
+
+        [Fact]
+        public void IsLeapYear_ForNumberNotDivisibleByFour_false()
+        {
+          LeapYear LeapYear = new LeapYear();
+          Assert.Equal(false, LeapYear.IsLeapYear(1999));
+        }
+        [Fact]
+        public void IsLeapYear_ForMultiplesOfOneHundred_false()
+        {
+          LeapYear LeapYear = new LeapYear();
+          Assert.Equal(false, LeapYear.IsLeapYear(1900));
+        }
+        [Fact]
+        public void IsLeapYear_ForMultiplesOfFourHundred_true()
+        {
+          LeapYear LeapYear = new LeapYear();
+          Assert.Equal(true, LeapYear.IsLeapYear(2000));
+        }
+    }
+}
